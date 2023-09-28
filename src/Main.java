3e12e4dc331e0092b2eea8eadeb76163aae7cfab
agtripp <trippeag@mail.uc.edu>
2023-09-28 15:38:14 -0400

    ShippingCost

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the price of the item: $");
+        double itemPrice = scanner.nextDouble();
+
+        double shippingCost;
+        if (itemPrice >= 100.0) {
+            shippingCost = 0.0;
+        } else {
+            shippingCost = 0.02 * itemPrice;
+        }
+
+        double totalPrice = itemPrice + shippingCost;
+
+        System.out.println("Shipping is: " + shippingCost);
+        System.out.println("Total Price is: "  + totalPrice);
+
+        scanner.close();
+    }
+}
